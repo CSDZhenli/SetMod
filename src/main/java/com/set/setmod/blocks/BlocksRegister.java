3daad723bb172882659ca67ea109344b12510cea
CSDzhenli <2567113342@qq.com>
2025-08-19 22:28:55 +0800

    加入下界反应核

--- src/main/java/com/set/setmod/blocks/BlocksRegister.java
+++ src/main/java/com/set/setmod/blocks/BlocksRegister.java
@@ -34,6 +34,7 @@ public class BlocksRegister {
     public static final Block BLOCK_8= new BlockBase("null1",Material.ROCK).setHardness(-1);
     public static final Block BLOCK_9= new BlockGlasses("glass",Material.ROCK).setHardness(-1);
     public static final Block BLOCK_10= new BlockNewBase("dirt",Material.ROCK,SoundType.GROUND).setHardness(1.5F);
+    public static final Block BLOCK_11= new BlockBase("netherreactor", Material.ROCK).setHardness(1F);
     @SubscribeEvent
     //注册方块
     public static void registerBlocks(RegistryEvent.Register<Block> event) {
