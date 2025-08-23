d0ef5f2aa8f30d67b8b3cd2e15b0ac5f3b3a39f5
CSDzhenli <2567113342@qq.com>
2025-08-24 02:20:32 +0800

    加入占位符方块

--- src/main/java/com/set/setmod/blocks/BlocksRegister.java
+++ src/main/java/com/set/setmod/blocks/BlocksRegister.java
@@ -37,6 +37,7 @@ public class BlocksRegister {
     public static final Block BLOCK_11= new BlockNetherReactor("netherreactor", Material.ROCK).setHardness(1F);
     public static final Block BLOCK_12= new BlockBase("glowingobsidian", Material.ROCK).setHardness(1F).setLightLevel(12F);
     public static final Block BLOCK_13= new BlockBase("netherreactora", Material.ROCK).setHardness(1F);
+    public static final Block BLOCK_14= new BlockFakeBase("update",Material.GRASS).setHardness(-1);
     @SubscribeEvent
     //注册方块
     public static void registerBlocks(RegistryEvent.Register<Block> event) {
